6833258d68d2723e6a5e4ef91ff71ec987f2b4f4
quantumke <nguruben@gmail.com>
2016-09-09 07:06:25 +0300

    slug handler

--- blog/views.py
+++ blog/views.py
@@ -31,3 +31,31 @@ def entries_index(request):
 
 
 
+def view_more(request, slug):
+	#count=Entry.objects.get(slug=slug)
+	next_issue = Entry.objects.get(slug=slug)
+	current_id= next_issue.pk
+	current_category=next_issue.tags
+	next_iem_id= current_id+1
+	last_id=Entry.objects.latest('id')
+	last_id= last_id.pk
+	if next_iem_id > last_id:
+		next_item = Entry.objects.get(id=current_id - 1)
+	else:
+		next_item = Entry.objects.get(id=next_iem_id)
+	next_item_slug= next_item.slug
+	next_item_title=next_item.title
+	similar= Entry.objects.filter(tags=current_category)
+	newest = Entry.objects.filter(featured=True)[:4]
+	marque = Entry.objects.all().order_by('-pub_date')[:3]
+	return render_to_response('entry_detail.html',{
+	'object':get_object_or_404(Entry, slug=slug),
+		'newest':newest,
+		'next_item_slug':next_item_slug,
+		'next_item_title':next_item_title,
+		'marque':marque,
+		'similar':similar,
+		'current_path': request.get_full_path,
+
+	},RequestContext(request))
+
